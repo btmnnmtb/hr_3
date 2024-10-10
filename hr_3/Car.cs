f60ce5fec9801420b70120f1ce9c2c9c0dbac2a7
Batman <kvashnin-2005@mail.ru>
2024-10-10 16:54:57 +0300

    Добавьте файлы проекта.

---
+++ hr_3/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hr_3
+{
+    internal class Car
+    {
+        public int CarId;
+        public int CarGodId;
+        public int CarGod { get; private set; }
+        public string CarName { get; private set; }
+        public Car(table_222 tab)
+        {
+            CarId = tab.id_table_22;
+            CarGod = (int)tab.car_god_proizvod;
+            if (tab.table_111 != null)
+            {
+                CarGodId = tab.table_111.id_table_11;
+                CarName = tab.table_111.car_name;
+            }
+
+        }
+
+
+    }
+}
---
+++ hr_3/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using hr_3.pr1poCCCDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hr_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        table_222TableAdapter table_111Adapter = new table_222TableAdapter();
+        private pr1poCCCEntities table = new pr1poCCCEntities();
+        List<Car> cars = new List<Car>();
+        public MainWindow()
+           
+        {
+            InitializeComponent();
+            table_2.ItemsSource = table.table_222.ToList(); // Для способа с привязкаии
+            foreach (var item in table.table_222.ToList())  // Для способа с классом
+            {
+                cars.Add(new Car(item));
+            }
+            table_1.ItemsSource = cars; // Конец способа с классом
+            table_3.ItemsSource = table_111Adapter.GetFullinfo(); // Способ для DataSet
+            
+        }
+            private void Window_Loaded(object sender , RoutedEventArgs e)
+            {
+
+                table_3.Columns[0].Visibility = Visibility.Collapsed; //Убираем 1 и 3 столбец
+                table_3.Columns[2].Visibility = Visibility.Collapsed;   
+            }//Конец способа для DataGrid
+    }
+}
